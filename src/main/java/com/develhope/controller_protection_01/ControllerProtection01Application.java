34b03878a0b61c3e584c36e2afd7bdcbfe7851a1
Mirko Di Cristina <mirko7726@gmail.com>
2022-11-29 10:37:35 +0100

    First Commit

---
+++ src/main/java/com/develhope/controller_protection_01/ControllerProtection01Application.java
@@ -0,0 +1,13 @@
+package com.develhope.controller_protection_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerProtection01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllerProtection01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/develhope/controller_protection_01/ControllerProtection01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develhope.controller_protection_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerProtection01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
